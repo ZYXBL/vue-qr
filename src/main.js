392b77a2af67475cb921314423e9f13838f6ef74
周阳 <zy825692497@163.com>
2020-05-18 09:48:49 +0800

    导出修改

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import VueBstcode from './vue-qr.vue'
-export default VueBstcode
+
+const install = function (Vue) {
+  if (install.installed) return;
+  install.installed
+  Vue.component('vue-code', VueBstcode)
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export default {
+  install
+}
